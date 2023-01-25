3b9ff8edfdbc50ed44dc8cddcdc24426226baf24
Richard Tallent <richard.tallent@erm.com>
2023-01-25 12:02:46 -0600

    Minimal example

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import Vue from "@vitejs/plugin-vue";
+import VueMacros from "unplugin-vue-macros/vite";
+
+export default defineConfig({
+	plugins: [
+		VueMacros({
+			plugins: {
+				vue: Vue({
+					reactivityTransform: true,
+				}),
+			},
+		}),
+	],
+});
